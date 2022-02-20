c6ab03d6b1c2f3e25af9a6386d2b8708432a0ef7
LolBaum <og.og.og.plo@gmail.com>
2022-02-20 13:05:39 +0100

    one step closer to a working Fontsystem

--- text.h
+++ text.h
@@ -2,6 +2,7 @@
 #include FT_FREETYPE_H
 
 #include "glm/glm.hpp"
+#include <glm/gtc/type_ptr.hpp>
 #include <glew.h>
 #include <iostream>
 #include <map>
@@ -34,8 +35,6 @@ int load_face(FT_Face face, FT_Library ft){
 	}
 
 	
-
-
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
   
 	for (unsigned char c = 0; c < 128; c++)
@@ -74,17 +73,14 @@ int load_face(FT_Face face, FT_Library ft){
 			(unsigned int)face->glyph->advance.x
 		};
 		Characters.insert(std::pair<char, Character>(c, character));
-		
 	}
 	return 0;
 }
 
 class Font{
-private:
-    
-    
+private:  
 public:
-    std::map<char, Character> Characters;
+    //std::map<char, Character> Characters;
     int size; 
     FT_Face face;
 
@@ -95,6 +91,8 @@ public:
             // todo: add function for error logging and error handling
         }
         load_face(face, ft);
+        FT_Done_Face(face);
+        //FT_Done_FreeType(ft);
         /* glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
         for (unsigned char c = 0; c < 128; c++){
             //load_character_glyph(c);
@@ -178,12 +176,17 @@ public:
 class FontSystem
 {
 private:
-    unsigned int VAO, VBO;
-    Shader font_shader = Shader("shaders/font.vs", "shaders/font.fs");
+    
+    
     glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(sdl_handler.getWidth()), 
                                       0.0f, static_cast<float>(sdl_handler.getHeight()));
+    bool loaded = false;
 public:
     FT_Library ft;
+
+    unsigned int VAO, VBO;
+    Shader font_shader = Shader("shaders/font.vs", "shaders/font.fs");
+
     FontSystem(){}
     int initialize(){
         if (FT_Init_FreeType(&ft)){
@@ -199,6 +202,9 @@ public:
         glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
+        glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(sdl_handler.getWidth()), 0.0f, static_cast<float>(sdl_handler.getHeight()));
+        font_shader.bind();
+        glUniformMatrix4fv(glGetUniformLocation(font_shader.getShaderId(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         return 0;
     }
 
@@ -217,7 +223,7 @@ public:
         std::string::const_iterator c;
         for (c = text.begin(); c != text.end(); c++)
         {
-            Character ch = font.Characters[*c];
+            Character ch = Characters[*c];
 
             float xpos = x + ch.Bearing.x * scale;
             float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
@@ -273,23 +279,87 @@ public:
         if (FT_Init_FreeType(&ft)){
             std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
         }
+        else{
+            loaded = true;
+        }
     }
     void unload(){
-        FT_Done_FreeType(ft);
+        if(loaded){
+            FT_Done_FreeType(ft);
+            loaded = false;
+        }
     }
-    ~FontSystem(){}
+    ~FontSystem(){
+        unload();
+    }
 };
 
 //FontSystem Fontsys = FontSystem();
 
 
+void RenderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color, int VAO, int VBO)
+{
+    // activate corresponding render state	
+    //s.Use();
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
+	s.bind();
+    //glUniform3f(glGetUniformLocation(s.Program, "textColor"), color.x, color.y, color.z);
+	glUniform3f(glGetUniformLocation(s.getShaderId(), "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
 
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
 
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
 
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },            
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
 
-
-
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }           
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+	s.unbind();
+	glDisable(GL_BLEND);
+}
 
 
 // String splitting from Stackoverflow https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
 
+void render_multiline_text(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color, int VAO, int VBO, float f_size, float line_offset=5){
+	std::string delimiter = "\n";
+	std::string line;
+	size_t last = 0; 
+	size_t next = 0; 
+	int i = 0;
+	while ((next = text.find(delimiter, last)) != std::string::npos){
+		line = text.substr(last, next-last);
+		RenderText(s, line, x, y-(line_offset+f_size*scale)*i, scale, color, VAO, VBO);
+		last = next + 1;
+		i++;
+	}
+}
