45599cd21d1f52c4c79548f57416159f6fdfe681
LolBaum <og.og.og.plo@gmail.com>
2022-02-22 21:08:35 +0100

    added Camera Height

--- fps_camera.h
+++ fps_camera.h
@@ -5,6 +5,7 @@ class FPSCamera : public Camera {
 public:
 	FPSCamera(float fov, float width, float height) : Camera(fov, width, height){
 		up = glm::vec3(0.0f, 1.0f, 0.0f);
+		camera_height = glm::vec3(0.0f, 1.7f, 0.0f);
 		yaw = -90.0f;
 		pitch = 0.0f;
 		onMouseMoved(0.0f, 0.0f);
@@ -27,10 +28,16 @@ public:
 		update();
 	}
 
-	void update() override {
+/* 	void update() override {
 		view = glm::lookAt(position, position + lookAt, up);
 		viewProj = projection * view;
+	} */
+
+	void update() override {
+		view = glm::lookAt(position+camera_height, position+camera_height + lookAt, up);
+		viewProj = projection * view;
 	}
+
 
 	void moveFront(float amount) {
 		translate(glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f) * lookAt) * amount);
@@ -46,10 +53,15 @@ public:
 		return lookAt;
 	}
 
+	glm::vec3 getViewPos() {
+		return position+camera_height;
+	}
+
 protected:
 	float yaw;
 	float pitch;
 	glm::vec3 lookAt;
 	const float mouseSentitivity = 0.3f;
 	glm::vec3 up;
+	glm::vec3 camera_height;
 };
--- screenshot.h
+++ screenshot.h
@@ -24,8 +24,6 @@ std::string numerate_name(std::string name, std::string ending=".png", int max_t
 	return new_name;
 }
 
-
-
 void savePNG(std::string filename, int windowWidth, int windowHeight, int num_chanels, unsigned char* pixels){
     int no_error = stbi_write_png(filename.c_str(), windowWidth, windowHeight, num_chanels, pixels, windowWidth * num_chanels);
     if (no_error){
@@ -43,20 +41,12 @@ void saveScreenshotToFile(std::string filename, int windowWidth, int windowHeigh
 
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
     glReadBuffer(GL_FRONT);
-    glReadPixels(0, 0, windowWidth, windowHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+    glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
 	int num_chanels = 3;
-	unsigned char *correctpixels = new unsigned char[numberOfPixels];
-	for(int i=0; i<numberOfPixels-2; i=i+3){
-		correctpixels[i] = pixels[i+2];
-		correctpixels[i+1] = pixels[i+1];
-		correctpixels[i+2] = pixels[i];
-	} 
-
 	stbi_flip_vertically_on_write(1);
-    savePNG(filename.c_str(), windowWidth, windowHeight, num_chanels, correctpixels);
+    savePNG(filename.c_str(), windowWidth, windowHeight, num_chanels, pixels);
 
 	delete[] pixels;
-	delete[] correctpixels;
 }
 
