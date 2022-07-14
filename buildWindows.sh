983b0f6c08c2ca1af0f41f4b704b39ff9dba5988
LolBaum <og.og.og.plo@gmail.com>
2022-07-14 20:56:37 +0200

    fixed buildWindows.sh

--- buildWindows.sh
+++ buildWindows.sh
@@ -24,12 +24,12 @@ $SOURCES = "$SRC_DIR\*.cpp" #,"$BASE_DIR\main.cpp", `
 
 # Including all directories which are needed
 $INCLUDES = "-I$LIB_DIR\SDL2\x86_64-w64-mingw32\include\SDL2", "-I$LIB_DIR\glew-2.1.0\include\GL", `
-"-I$LIB_DIR\glm", "-I$LIB_DIR\freetype-win\include", "-I$LIB_DIR\include", "-I$include_DIR" ###, "-I$IMGUI_DIR", "-I$IMGUI_DIR\backends"
+"-I$LIB_DIR\glm", "-I$LIB_DIR\include", "-I$include_DIR" ###, "-I$LIB_DIR\freetype-win\include",  "-I$IMGUI_DIR", "-I$IMGUI_DIR\backends"
 
 # linking the libraries
 $LINKER = "-L$LIB_DIR\SDL2\x86_64-w64-mingw32\lib", "-L$LIB_DIR\glew-2.1.0\lib\Release\x64", "-L$LIB_DIR\freetype-win\release_dll\win64"
 $LIBS = "-lgdi32", "-limm32", "-lglew32", "-lmingw32", "-lSDL2main", "-lSDL2", "-lglew32", "-lopengl32", "-lWs2_32", `
-"-lole32", "-lcomctl32", "-lgdi32", "-lcomdlg32", "-luuid", "-lfreetype"
+"-lole32", "-lcomctl32", "-lgdi32", "-lcomdlg32", "-luuid" ###, "-lfreetype"
 # REMEMBER: NO Whithespaces inside the ""
 
 # Compilation
