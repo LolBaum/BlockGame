6d46dfb0effebd4ec83b39478c6402e44ab245a8
LolBaum <og.og.og.plo@gmail.com>
2024-12-10 19:19:48 +0100

    wip: ON DRUGS

--- build/shaders/frame.fs
+++ build/shaders/frame.fs
@@ -8,6 +8,7 @@ const float width = 1920.0f;
 const float height = 1080.0f;
 
 
+
 layout (binding = 0) uniform sampler2D screenTexture;
 
 // revealage threshold buffer
@@ -128,33 +129,35 @@ void main()
 
 
 // TRY THIS:
-//     float Pi = 6.28318530718; // Pi*2
-//
-//     // GAUSSIAN BLUR SETTINGS {{{
-//     float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
-//     float Quality = 3.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
-//     float Size = 8.0; // BLUR SIZE (Radius)
-//     // GAUSSIAN BLUR SETTINGS }}}
-//
-//     vec2 Radius = Size/iResolution.xy;
-//
-//     // Normalized pixel coordinates (from 0 to 1)
-//     vec2 uv = fragCoord/iResolution.xy;
-//     // Pixel colour
-//     vec4 Color = texture(iChannel0, uv);
-//
-//     // Blur calculations
-//     for( float d=0.0; d<Pi; d+=Pi/Directions)
-//     {
-// 		for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
-//         {
-// 			Color += texture( iChannel0, uv+vec2(cos(d),sin(d))*Radius*i);
-//         }
-//     }
-//
-//     // Output to screen
-//     Color /= Quality * Directions - 15.0;
-//     fragColor =  Color;
+    float Pi = 6.28318530718; // Pi*2
+
+    // GAUSSIAN BLUR SETTINGS {{{
+    float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
+    float Quality = 3.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
+    float Size = 8.0; // BLUR SIZE (Radius)
+    // GAUSSIAN BLUR SETTINGS }}}
+
+    vec2 iResolution = vec2(1, 1);
+
+    vec2 Radius = Size/iResolution.xy;
+
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = TexCoords/iResolution.xy;
+    // Pixel colour
+    vec4 Color = texture(screenTexture, uv);
+
+    // Blur calculations
+    for( float d=0.0; d<Pi; d+=Pi/Directions)
+    {
+		for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
+        {
+			Color += texture( screenTexture, uv+vec2(cos(d),sin(d))*Radius*i);
+        }
+    }
+
+    // Output to screen
+    Color /= Quality * Directions - 15.0;
+    FragColor =  Color;
 
 
     if (texture(u_texture_gui, TexCoords).a > 0.1){
